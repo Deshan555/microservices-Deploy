09a56c97bb8c69745a7af4a033464192dc2667ab
Bloodraven <74134064+Deshan555@users.noreply.github.com>
2025-01-04 16:42:06 +0530

    Add express-status-monitor middleware for server health monitoring

--- src/Server.js
+++ src/Server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const endPoints = require('./routes/Routes');
 const logger = require('morgan');
 const cors = require('cors');
+const statusMonitor = require('express-status-monitor'); // Add this line
 
 const server = express();
 const port = 3001;
@@ -13,17 +14,10 @@ server.use(cors());
 
 server.use(bodyParser.json());
 
+server.use(statusMonitor()); // Add this line
+
 server.use('/thaprobane/core/v01', endPoints);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-// cors modifications in the server.js file
-// server.use(cors({
-//     origin: 'http://your_frontend_domain.com',  // Replace with the domain of your frontend
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,  // Enable credentials (cookies, authorization headers, etc.)
-//     optionsSuccessStatus: 204,  // Some legacy browsers (IE11, various SmartTVs) choke on 204
-// }));
